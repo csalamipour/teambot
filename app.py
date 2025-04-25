61e5fe75022908bc2ac280d0cc2747d9600e4fc2
abhikatoldtrafford <abhikatoldtrafford@users.noreply.github.com>
2025-04-25 19:20:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -352,5 +352,5 @@ async def root():
 # Run the app with uvicorn if executed directly
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
